2d5d8e604c382e02b6bd4d95c1286dfc4b7cebca
Zipra1 <80921627+Zipra1@users.noreply.github.com>
2021-03-18 19:20:34 -0400

    Add files via upload

---
+++ Blockify/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Net;
+using System.IO;
+using System.Text;
+
+namespace Blockify
+{
+    public partial class Form1 : Form
+    {
+        private readonly string spotifyPath = Environment.GetEnvironmentVariable("APPDATA") + @"\Spotify\spotify.exe";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string version = "1.0.0.9"; // The version of blockify to compare with the current version on my server. If you're modifying Blockify, change this to "MOD"
+
+            public const int KEYEVENTF_EXTENTEDKEY = 1;
+            public const int KEYEVENTF_KEYUP = 0;
+            public const int VK_MEDIA_NEXT_TRACK = 0xB0;
+            public const int VK_MEDIA_PLAY_PAUSE = 0xB3;
+            public const int VK_MEDIA_PREV_TRACK = 0xB1;
+
+            [DllImport("user32.dll")]
+            public static extern void keybd_event(byte virtualKey, byte scanCode, uint flags, IntPtr extraInfo);
+
+        [DllImport("user32.dll")]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        private IntPtr brwin;
+
+        int skipAd = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            skipAd = 1;
+        }
+
+        int i = 1;
+        int lpuid = 0;
+        int dbg = 0;
+        int dbgo = 1;
+        private void timer2_Tick_1(object sender, EventArgs e)
+        {
+            brwin = GetForegroundWindow();
+            Process[] processlist = Process.GetProcesses();
+            Color darkRed = Color.FromName("DarkRed");
+            if (dbgo==2)
+            {
+                this.Size = new Size(319, 261 + trackBar4.Value);
+            }
+            //Finding and setting PUID of spotify's named window. (As a later discovery, this is not the PUID. lPUID refers to what I call the "local PUID", as I'm not sure what to call this number.)
+            try
+            {
+                if (processlist[i].MainWindowTitle.Contains("Advertisement") || processlist[i].MainWindowTitle == "Spotify")
+                {
+                    lpuid = i;
+                    label1.ForeColor = Color.FromArgb(255, 255 / dbgo, 255 / dbgo, 255);
+                } else if (processlist[i].MainWindowTitle.Contains("Spotify Free"))
+                {
+                    lpuid = i;
+                    label1.ForeColor = Color.FromArgb(255, 255 / dbgo, 255 / dbgo, 255);
+                } else if (processlist[i].MainWindowTitle.Contains(" - ") && !processlist[i].MainWindowTitle.Contains("Discord") && !processlist[i].MainWindowTitle.Contains("Microsoft Visual Studio") && !processlist[i].MainWindowTitle.Contains("Google Chrome") && !processlist[i].MainWindowTitle.Contains("Inbox - Sympatico") && !processlist[i].MainWindowTitle.Contains(" - Notepad") && !processlist[i].MainWindowTitle.Contains(" - Singleplayer"))
+                {
+                    lpuid = i;
+                    label1.ForeColor = Color.FromArgb(255, 255 / dbgo, 255 / dbgo, 255);
+                }
+                if (processlist[lpuid].MainWindowTitle.Contains("Advertisement") || processlist[i].MainWindowTitle == "Spotify" || skipAd == 1) {
+                    skipAd = 0;
+                    label1.ForeColor = darkRed;
+                    label1.Text = "Advertisement";
+                    ////Skipping Ad////
+                        foreach (Process proc in Process.GetProcessesByName("Spotify"))
+                        {
+                            brwin = GetForegroundWindow();
+                            label1.ForeColor = darkRed;
+                            proc.CloseMainWindow();
+                            Thread.Sleep(trackBar3.Value); // Kill buffer
+                            proc.Kill();
+                            proc.WaitForExit();
+                            SetForegroundWindow(brwin);
+                        }
+                        //Restarting Spotify
+                        Thread.Sleep(trackBar1.Value); // Restart buffer
+                        using (Process myProcess = new Process())
+                        {
+                        myProcess.StartInfo.FileName = spotifyPath;
+                        myProcess.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal;
+                        myProcess.Start();
+                        SetForegroundWindow(brwin);
+                        Thread.Sleep(100);
+                        }
+
+                    Thread.Sleep(trackBar2.Value);
+                    keybd_event(VK_MEDIA_PLAY_PAUSE, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero); // Unpause Spotify
+                    SetForegroundWindow(brwin); // Attempt to return focus to the user's previous window.
+                ////End of Skipping Ad////
+            }
+            else if(processlist[lpuid].MainWindowTitle.Contains("Spotify Free")){
+                label1.Text = "Paused";
+            }else if (processlist[lpuid].MainWindowTitle.Contains(" - ") && !processlist[i].MainWindowTitle.Contains("Discord") && !processlist[i].MainWindowTitle.Contains("Microsoft Visual Studio") && !processlist[i].MainWindowTitle.Contains("Google Chrome") && !processlist[i].MainWindowTitle.Contains("Inbox - Sympatico"))
+            {
+                label1.Text = processlist[lpuid].MainWindowTitle;
+            }
+            }
+            catch (Exception exc) {
+                if (dbgo == 2){
+                    if (!checkBox2.Checked)
+                    {
+                        label5.Text = "\n\n" + System.DateTime.Now + ": " + Convert.ToString(exc) + label5.Text;
+                    }
+                }
+                if (Convert.ToString(exc).Contains("System.Diagnostics.Process.Kill"))
+                {
+                    using (Process myProcess = new Process())
+                    {
+                        label5.Text = "\n\n" + System.DateTime.Now + ": " + "Remedying kill error" + label5.Text;
+                        myProcess.StartInfo.FileName = spotifyPath;
+                        myProcess.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal;
+                        myProcess.Start();
+                        SetForegroundWindow(brwin); // Attempt to return focus to the user's previous window again.
+                        Thread.Sleep(trackBar2.Value);
+                        keybd_event(VK_MEDIA_PLAY_PAUSE, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero); // Unpause Spotify
+                        SetForegroundWindow(brwin); // Attempt to return focus to the user's previous window again.
+                    }
+                }
+            }
+            if (i > processlist.Length)
+            {
+                i = 1;
+            }
+            i = i + 1;
+
+            //Trackbar value feedback
+            label3.Text = "P Buffer: " + Convert.ToString(trackBar2.Value);
+            label4.Text = "K Buffer: " + Convert.ToString(trackBar3.Value);
+            label2.Text = "R Buffer: " + Convert.ToString(trackBar1.Value);
+
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                notifyIcon1.Visible = true;
+            }
+
+        }
+
+            private void button2_Click(object sender, EventArgs e) // Info Button
+        {
+            Form form2 = new Form2();
+            form2.StartPosition = FormStartPosition.CenterParent;
+            form2.ShowDialog();
+        }
+
+        ///###############///
+        ///Saving Settings///
+        ///###############///
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetSettings(); // Get settings on startup
+            CheckUpdate(); // Check for an update from server
+            this.Size = new Size(319, 161);
+        }
+
+        public void GetSettings() // Get settings of each value
+        {
+            try
+            {
+                trackBar3.Value = Properties.Settings.Default.KBfr;
+                trackBar2.Value = Properties.Settings.Default.PBfr;
+                trackBar1.Value = Properties.Settings.Default.RBfr;
+            }
+            catch { }
+        }
+        public void CheckUpdate() // Get settings of each value
+        {
+            try
+            {
+
+                //var client = new WebClient();
+                //client.Proxy = null;
+                //string url = "http://192.168.2.200/";
+
+                //string url = "http://192.168.2.200/"; This is here for testing the program on my network.
+                //http://blockify.mooo.com/ This is also here for testing on my own network, just to copy and paste quickly.
+                //string content = client.DownloadString(url);
+
+                string urlAddress = "http://blockify.mooo.com/";
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+                request.Timeout = 1000;
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    Stream receiveStream = response.GetResponseStream();
+                    StreamReader readStream = null;
+
+                    if (String.IsNullOrWhiteSpace(response.CharacterSet))
+                        readStream = new StreamReader(receiveStream);
+                    else
+                        readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+
+                    string data = readStream.ReadToEnd();
+                    if (!data.Contains(version) && version != "MOD")
+                    {
+                        Form form3 = new Form3();
+                        form3.StartPosition = FormStartPosition.CenterParent;
+                        form3.ShowDialog();
+
+                    }
+
+                    response.Close();
+                    readStream.Close();
+                }
+                //!content.Contains(version) && 
+            }
+            catch (Exception exc2)
+            {
+                label1.Text = Convert.ToString(exc2);
+            }
+        }
+        public void SaveSettings() // Save settings of each value
+        {
+            Properties.Settings.Default.KBfr = trackBar3.Value;
+            Properties.Settings.Default.PBfr = trackBar2.Value;
+            Properties.Settings.Default.RBfr = trackBar1.Value;
+            Properties.Settings.Default.Save();
+        }
+        private void button3_Click(object sender, EventArgs e) // Run "SaveSettings" when Save button is pressed.
+        {
+            SaveSettings();
+        }
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+        public void label1_Click(object sender, EventArgs e)
+        {
+            dbg++;
+            if (dbg > 2) {
+                dbgo = 2;
+                this.Size = new Size(319, 261+trackBar4.Value);
+                this.MaximizeBox = true;
+            }
+        }
+    }
+}
+
+//TODO
+// Focus issues
+// Mute / Block ads button
+// Fix false detections: Add more &&!'s, keep them specific as to not call random songs Paused. // In progress
+// Spotify closes then doesn't open
+// Only unpause songs if audio is not playing after P buffer is over. This is because sometimes the song will not be paused upon restart, and when it is meant to be unpaused it is instead paused.
---
+++ Blockify/Form3.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace Blockify
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button_1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(158, 26);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "An update is available.\r\nYou can download it on GitHub.";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(89, 44);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 29);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Skip current ad";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button_1
+            // 
+            this.button_1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.button_1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_1.ForeColor = System.Drawing.Color.White;
+            this.button_1.Image = ((System.Drawing.Image)(resources.GetObject("button_1.Image")));
+            this.button_1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button_1.Location = new System.Drawing.Point(15, 44);
+            this.button_1.Name = "button_1";
+            this.button_1.Size = new System.Drawing.Size(68, 29);
+            this.button_1.TabIndex = 11;
+            this.button_1.Text = "GitHub";
+            this.button_1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button_1.UseVisualStyleBackColor = false;
+            this.button_1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.ClientSize = new System.Drawing.Size(189, 88);
+            this.Controls.Add(this.button_1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form3";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button_1;
+    }
+}
